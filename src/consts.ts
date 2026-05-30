8efbb4715ef34c8229c4088e71165d71bf1d928e
Thomas Custer <thomas.s.custer@gmail.com>
2026-05-30 12:01:16 -0400

    docs: clearly defined name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "BiP";
-export const SITE_DESCRIPTION = "Welcome to BiP!";
+export const SITE_TITLE = "Blog-in-Progress";
+export const SITE_DESCRIPTION = "Welcome to Blog-in-Progress BiP!";
